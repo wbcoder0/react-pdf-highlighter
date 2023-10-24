623d96c74f5963f77e67de7bd2773e846cd12d4d
wubo <wubobuaa@gmail.com>
2023-10-24 15:34:04 +0800

    fix: selection.isCollapsed

--- src/components/PdfHighlighter.tsx
+++ src/components/PdfHighlighter.tsx
@@ -431,6 +431,7 @@ export class PdfHighlighter<T_HT extends IHighlight> extends PureComponent<
 
     if (selection.isCollapsed) {
       this.setState({ isCollapsed: true });
+      this.hideTipAndSelection();
       return;
     }
 
